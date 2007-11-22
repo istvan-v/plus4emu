bd9d3e49c1f628535349a9fdd313dcb5ee64b351
istvanv <istvanv@8e1dae93-d52d-0410-bca0-ce40f4ba621a>
2007-11-22 15:28:58 +0000

    Allow video recording in RLE8 format

--- src/videorec.hpp
+++ src/videorec.hpp
@@ -32,7 +32,7 @@ namespace Plus4Emu {
     static const int  videoHeight = 288;
     static const int  sampleRate = 48000;
     static const int  audioBuffers = 8;
-   private:
+   protected:
     class AudioConverter_ : public AudioConverterHighQuality {
      private:
       VideoCapture& videoCapture;
@@ -45,22 +45,10 @@ namespace Plus4Emu {
      protected:
       virtual void audioOutput(int16_t outputSignal_);
     };
+    // --------
     std::FILE   *aviFile;
     uint8_t     *lineBuf;               // 720 bytes
-    uint8_t     *frameBuf0Y;            // 384x288
-    uint8_t     *frameBuf0V;            // 192x144
-    uint8_t     *frameBuf0U;            // 192x144
-    uint8_t     *frameBuf1Y;            // 384x288
-    uint8_t     *frameBuf1V;            // 192x144
-    uint8_t     *frameBuf1U;            // 192x144
-    int32_t     *interpBufY;            // 384x288
-    int32_t     *interpBufV;            // 192x144
-    int32_t     *interpBufU;            // 192x144
-    uint8_t     *outBufY;               // 384x288
-    uint8_t     *outBufV;               // 192x144
-    uint8_t     *outBufU;               // 192x144
     int16_t     *audioBuf;              // 8 * (sampleRate / frameRate) samples
-    uint8_t     *duplicateFrameBitmap;
     int         frameRate;              // video frames per second
     int         audioBufSize;           // = (sampleRate / frameRate)
     int         audioBufReadPos;
@@ -100,32 +88,28 @@ namespace Plus4Emu {
     size_t      duplicateFrames;
     size_t      fileSize;
     VideoDisplay::DisplayParameters displayParameters;
-    AudioConverter                  *audioConverter;
-    VideoDisplayColormap<uint32_t>  colormap;
+    AudioConverter  *audioConverter;
+    size_t      aviHeaderSize;
     void        (*errorCallback)(void *userData, const char *msg);
     void        *errorCallbackUserData;
     void        (*fileNameCallback)(void *userData, std::string& fileName);
     void        *fileNameCallbackUserData;
     // ----------------
-    void lineDone();
-    void decodeLine();
-    void frameDone();
-    void resampleFrame();
-    void writeFrame(bool frameChanged);
-    void writeAVIHeader();
-    void writeAVIIndex();
-    void closeFile();
-    void errorMessage(const char *msg);
     static void aviHeader_writeFourCC(uint8_t*& bufp, const char *s);
     static void aviHeader_writeUInt16(uint8_t*& bufp, uint16_t n);
     static void aviHeader_writeUInt32(uint8_t*& bufp, uint32_t n);
     static void defaultErrorCallback(void *userData, const char *msg);
     static void defaultFileNameCallback(void *userData, std::string& fileName);
+    virtual void decodeLine(int lineNum) = 0;
+    virtual void clearLine(int lineNum) = 0;
+    virtual void frameDone() = 0;
+    virtual void writeAVIHeader() = 0;
+    virtual void writeAVIIndex() = 0;
+    void lineDone();
+    void closeFile();
+    void errorMessage(const char *msg);
    public:
-    VideoCapture(void indexToYUVFunc(uint8_t color, bool isNTSC,
-                                     float& y, float& u, float& v) =
-                     (void (*)(uint8_t, bool, float&, float&, float&)) 0,
-                 int frameRate_ = 30);
+    VideoCapture(int frameRate_ = 50);
     virtual ~VideoCapture();
     void runOneCycle(const uint8_t *videoInput, int16_t audioInput);
     void setClockFrequency(size_t freq_);
@@ -136,6 +120,103 @@ namespace Plus4Emu {
     void setFileNameCallback(void (*func)(void *userData,
                                           std::string& fileName),
                              void *userData_);
+  };
+
+  // --------------------------------------------------------------------------
+
+  class VideoCapture_RLE8 : public VideoCapture {
+   private:
+    class VideoCaptureFrameBuffer {
+     private:
+      uint32_t  *buf;
+      uint8_t   **linePtrs;
+      uint32_t  *lineBytes_;
+     public:
+      VideoCaptureFrameBuffer(int w, int h);
+      virtual ~VideoCaptureFrameBuffer();
+      inline uint8_t * operator[](long n)
+      {
+        return linePtrs[n];
+      }
+      inline const uint8_t * operator[](long n) const
+      {
+        return linePtrs[n];
+      }
+      inline uint32_t& lineBytes(long n)
+      {
+        return lineBytes_[n];
+      }
+      inline const uint32_t& lineBytes(long n) const
+      {
+        return lineBytes_[n];
+      }
+      // returns true if the lines are identical
+      inline bool compareLine(long dstLine,
+                              const VideoCaptureFrameBuffer& src, long srcLine);
+      inline void copyLine(long dstLine, long srcLine);
+      inline void copyLine(long dstLine,
+                           const VideoCaptureFrameBuffer& src, long srcLine);
+      inline void clearLine(long n);
+    };
+    // --------
+    VideoCaptureFrameBuffer tmpFrameBuf;    // 384x288
+    VideoCaptureFrameBuffer outputFrameBuf; // 384x288
+    uint32_t    *frameSizes;
+    int         cycleCnt;
+    uint8_t     *colormap;      // 512 entries, 0..255: PAL, 256..511: NTSC
+    // ----------------
+   protected:
+    virtual void decodeLine(int lineNum);
+    virtual void clearLine(int lineNum);
+    virtual void frameDone();
+    virtual void writeAVIHeader();
+    virtual void writeAVIIndex();
+   private:
+    size_t rleCompressLine(uint8_t *outBuf, const uint8_t *inBuf);
+    void writeFrame(bool frameChanged);
+   public:
+    VideoCapture_RLE8(void indexToYUVFunc(uint8_t color, bool isNTSC,
+                                          float& y, float& u, float& v) =
+                          (void (*)(uint8_t, bool, float&, float&, float&)) 0,
+                      int frameRate_ = 50);
+    virtual ~VideoCapture_RLE8();
+  };
+
+  // --------------------------------------------------------------------------
+
+  class VideoCapture_YV12 : public VideoCapture {
+   private:
+    uint32_t    *videoBuf;              // space for all YUV video data:
+    uint8_t     *frameBuf0Y;            // 384x288
+    uint8_t     *frameBuf0V;            // 192x144
+    uint8_t     *frameBuf0U;            // 192x144
+    uint8_t     *frameBuf1Y;            // 384x288
+    uint8_t     *frameBuf1V;            // 192x144
+    uint8_t     *frameBuf1U;            // 192x144
+    int32_t     *interpBufY;            // 384x288
+    int32_t     *interpBufV;            // 192x144
+    int32_t     *interpBufU;            // 192x144
+    uint8_t     *outBufY;               // 384x288
+    uint8_t     *outBufV;               // 192x144
+    uint8_t     *outBufU;               // 192x144
+    uint8_t     *duplicateFrameBitmap;
+    VideoDisplayColormap<uint32_t>  colormap;
+    // ----------------
+   protected:
+    virtual void decodeLine(int lineNum);
+    virtual void clearLine(int lineNum);
+    virtual void frameDone();
+    virtual void writeAVIHeader();
+    virtual void writeAVIIndex();
+   private:
+    void resampleFrame();
+    void writeFrame(bool frameChanged);
+   public:
+    VideoCapture_YV12(void indexToYUVFunc(uint8_t color, bool isNTSC,
+                                          float& y, float& u, float& v) =
+                          (void (*)(uint8_t, bool, float&, float&, float&)) 0,
+                      int frameRate_ = 30);
+    virtual ~VideoCapture_YV12();
   };
 
 }       // namespace Plus4Emu
