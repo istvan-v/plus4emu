530d9e8ee2ff16e0803fadb8ecca914c2a432704
istvanv <istvanv@8e1dae93-d52d-0410-bca0-ce40f4ba621a>
2008-05-22 20:13:34 +0000

    Minor changes

--- src/gldisp.hpp
+++ src/gldisp.hpp
@@ -1,6 +1,6 @@
 
 // plus4emu -- portable Commodore Plus/4 emulator
-// Copyright (C) 2003-2007 Istvan Varga <istvanv@users.sourceforge.net>
+// Copyright (C) 2003-2008 Istvan Varga <istvanv@users.sourceforge.net>
 // http://sourceforge.net/projects/plus4emu/
 //
 // This program is free software; you can redistribute it and/or modify
