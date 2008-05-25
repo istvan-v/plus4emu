7cea84f399ee0f4762677cd8773f5443b39ccbe5
istvanv <istvanv@8e1dae93-d52d-0410-bca0-ce40f4ba621a>
2008-05-25 16:08:04 +0000

    1.2.6.1 release

--- gui/gui.cpp
+++ gui/gui.cpp
@@ -114,10 +114,10 @@ void Plus4EmuGUI::init_()
 void Plus4EmuGUI::updateDisplay_windowTitle()
 {
   if (oldPauseFlag) {
-    std::sprintf(&(windowTitleBuf[0]), "plus4emu 1.2.6.1 beta (paused)");
+    std::sprintf(&(windowTitleBuf[0]), "plus4emu 1.2.6.1 (paused)");
   }
   else {
-    std::sprintf(&(windowTitleBuf[0]), "plus4emu 1.2.6.1 beta (%d%%)",
+    std::sprintf(&(windowTitleBuf[0]), "plus4emu 1.2.6.1 (%d%%)",
                  int(oldSpeedPercentage));
   }
   mainWindow->label(&(windowTitleBuf[0]));
--- installer/plus4emu.nsi
+++ installer/plus4emu.nsi
@@ -14,7 +14,7 @@
 
   ;Name and file
   Name "plus4emu"
-  OutFile "plus4emu-1.2.6.1-beta.exe"
+  OutFile "plus4emu-1.2.6.1.exe"
 
   ;Default installation folder
   InstallDir "$PROGRAMFILES\plus4emu"
